e3bfdf3b506a45ca3b2a4003bbb27fd2f98cb1d0
Nye32 <nyeveandy@gmail.com>
2015-01-25 16:51:36 -0500

    Was able to get thruster running off PS3 controller. Fun Times.

---
+++ catkin_generated/setup_cached.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+export CATKIN_TEST_RESULTS_DIR="/home/andrew/catkin_ws/src/controller_test/test_results"
+export ROS_TEST_RESULTS_DIR="/home/andrew/catkin_ws/src/controller_test/test_results"
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/andrew/catkin_ws/src/controller_test/devel:$CMAKE_PREFIX_PATH"
+export CPATH="/home/andrew/catkin_ws/src/controller_test/devel/include:$CPATH"
+export LD_LIBRARY_PATH="/home/andrew/catkin_ws/src/controller_test/devel/lib:/home/andrew/catkin_ws/src/controller_test/devel/lib/x86_64-linux-gnu:$LD_LIBRARY_PATH"
+export PATH="/home/andrew/catkin_ws/src/controller_test/devel/bin:$PATH"
+export PKG_CONFIG_PATH="/home/andrew/catkin_ws/src/controller_test/devel/lib/pkgconfig:/home/andrew/catkin_ws/src/controller_test/devel/lib/x86_64-linux-gnu/pkgconfig:$PKG_CONFIG_PATH"
+export PYTHONPATH="/home/andrew/catkin_ws/src/controller_test/devel/lib/python2.7/dist-packages:$PYTHONPATH"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/andrew/catkin_ws/src/controller_test/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/andrew/catkin_ws/src/controller_test:$ROS_PACKAGE_PATH"
---
+++ devel/etc/catkin/profile.d/05.catkin-test-results.sh
@@ -0,0 +1,4 @@
+# generated from catkin/cmake/env-hooks/05.catkin-test-results.sh.develspace.in
+
+export CATKIN_TEST_RESULTS_DIR="/home/andrew/catkin_ws/src/controller_test/test_results"
+export ROS_TEST_RESULTS_DIR="$CATKIN_TEST_RESULTS_DIR"
