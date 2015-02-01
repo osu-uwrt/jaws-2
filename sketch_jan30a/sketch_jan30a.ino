cf771743ee4a76c05765d0cd6feb1607b16a1f19
chilypepper <peter.a.edin@gmail.com>
2015-02-01 18:06:10 -0500

    Up to date Arbotix Sketch.

--- sketch_jan30a/sketch_jan30a.ino
+++ sketch_jan30a/sketch_jan30a.ino
@@ -8,31 +8,64 @@
 void setup(){
    Serial.begin(38400); 
 
-    
-//Conor claims thrusters work this simply
     //thrustOne.attach(1);//arbitrary pin#
     //thrustTwo.attach(2);//arbitrary pin#
     //thrustThree.attach(3);//arbitrary pin# 
 }
 void loop(){
     int i;
-    int input[5];
-    while(Serial.available()<5){
-      for (int i=0;i<5;i++){
-       input[i]=Serial.read(); 
+    int input[7];
+    int temp;
+    delay(500);
+    int characters[15];
+    Serial.print(Serial.available()+" ");
+    if(Serial.peek()==45){
+      Serial.read();
+      while(1){
+        if(Serial.available()==20){//will change to accomadate \n
+           Serial.print("In the if");
+           for (int i=0;i<15;i++){
+             int temp=(int)Serial.read();
+             if(temp!=42 && temp!=38){
+               characters[i]=temp;
+             }
+             else{
+              characters[i]=(int)Serial.read(); 
+             }
+           }
+           break;
+        }
       }
-      //implement delay later on with the math
-      SetPosition(servoNumOne,input[0]);
-      SetPosition(servoNumTwo,input[1]);
-      
-      thrustOne.write(input[2]);
-      thrustTwo.write(input[3]);
-      thrustThree.write(input[4]);
-      
     }
+    else{
+     Serial.println(Serial.read()); 
+    }
+      int* inputs = arrayToInt(characters);
+      SetPosition(servoNumOne,inputs[0]);
+      SetPosition(servoNumTwo,inputs[1]);
+      
+      thrustOne.write(inputs[2]);
+      thrustTwo.write(inputs[3]);
+      thrustThree.write(inputs[4]);
+    }
+      
     
-    //Something nifty to think about:
-    //Arbotix cant support 2 traditional PWM servos,
-    //meaning our three thrusters might run into a problem with that
-    
-}
+   int* arrayToInt(int list[]){
+     String int1 = (String)list[0]+(String)list[1]+(String)list[2];
+     int servoOne=int1.toInt();
+     
+     String int2 = (String)list[3]+(String)list[4]+(String)list[5];
+     int servoTwo=int1.toInt();
+     
+     String int3 = (String)list[6]+(String)list[7]+(String)list[8];
+     int thrustOne=int1.toInt();
+     
+     String int4 = (String)list[9]+list[10]+list[11];
+     int thrustTwo=int1.toInt();
+     
+     String int5 = (String)list[12]+list[13]+list[14];
+     int thrustThree=int1.toInt();
+     
+     int returnVal[5] = {servoOne, servoTwo, thrustOne, thrustTwo, thrustThree};
+     return returnVal;
+   }
