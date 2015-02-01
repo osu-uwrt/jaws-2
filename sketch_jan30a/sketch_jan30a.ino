b0673ea01d6386dc8896ddd2eea45a6525e53b21
chilypepper <peter.a.edin@gmail.com>
2015-02-01 11:37:59 -0500

    Made the arbotix work

--- sketch_jan30a/sketch_jan30a.ino
+++ sketch_jan30a/sketch_jan30a.ino
@@ -1,31 +1,33 @@
-   
-   
-   
-    int servoNumOne=0;//forgot which ones i set them as
-    int servoNumTwo=15;//this number sounds familiar
+   #include <ax12.h>
+   #include <Servo.h>
+   Servo thrustOne;
+   Servo thrustTwo;
+   Servo thrustThree;
+   int servoNumOne=0;//forgot which ones i set them as
+   int servoNumTwo=15;//this number sounds familiar
 void setup(){
-    Serial.begin(38400); 
+   Serial.begin(38400); 
 
     
-   // Servo thrustOne;
-   // Servo thrustTwo;
-   // Servo thrustThree;//Conor claims thrusters work this simply
+//Conor claims thrusters work this simply
     //thrustOne.attach(1);//arbitrary pin#
     //thrustTwo.attach(2);//arbitrary pin#
     //thrustThree.attach(3);//arbitrary pin# 
 }
 void loop(){
     int i;
-    int input[6];
-    while(Serial.available()>0){
-      input = Serial.read();
+    int input[5];
+    while(Serial.available()<5){
+      for (int i=0;i<5;i++){
+       input[i]=Serial.read(); 
+      }
       //implement delay later on with the math
       SetPosition(servoNumOne,input[0]);
       SetPosition(servoNumTwo,input[1]);
       
-     // thrustOne.write(input[2]);
-   //   thrustTwo.write(input[3]);
-     // thrustThree.write(input[4]);
+      thrustOne.write(input[2]);
+      thrustTwo.write(input[3]);
+      thrustThree.write(input[4]);
       
     }
     
