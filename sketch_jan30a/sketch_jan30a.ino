53403090c03f03c4b566c3f3e6cd4c631eedeba2
chilypepper <peter.a.edin@gmail.com>
2015-02-06 14:03:03 -0500

    Should read Serial correctly now.

--- sketch_jan30a/sketch_jan30a.ino
+++ sketch_jan30a/sketch_jan30a.ino
@@ -1,12 +1,11 @@
-   
    #include <ax12.h>
    #include <Servo.h>
    
    Servo thrustOne;
    Servo thrustTwo;
    Servo thrustThree;
-   int servoNumOne=0;//forgot which ones i set them as
-   int servoNumTwo=15;//this number sounds familiar
+   int servoNumOne=18;//forgot which ones i set them as
+   int servoNumTwo=19;//this number sounds familiar
 void setup(){
    Serial.begin(38400); 
 
@@ -17,9 +16,10 @@ void setup(){
 void loop(){
     int i;
     int input[7];
+    String finals[5];
     int temp;
     delay(500);
-    int characters[20];
+    int sequence[15];
     Serial.print(Serial.available()+" ");
     if(Serial.peek()==45){
       Serial.read();
@@ -27,48 +27,31 @@ void loop(){
         if(Serial.available()==20){
            Serial.print("In the if");
            for (int i=0;i<20;i++){
-             char temp2=(int)Serial.read();
-             int temp=(int)temp2;
+             int temp2=(Serial.read()-'/');
              if(temp!=42 && temp!=38){
-               characters[i]=temp;
-             }
-             else{
-              characters[i]=(int)Serial.read(); 
+               sequence[i]=temp2;
              }
            }
            break;
         }
       }
-    }
-    else{
-     Serial.println(Serial.read()); 
-    }
-      int* inputs = arrayToInt(characters);
-      SetPosition(servoNumOne,inputs[0]);
-      SetPosition(servoNumTwo,inputs[1]);
+     finals[0]=String(sequence[0])+String(sequence[1])+String(sequence[2]);
+     finals[1]=String(sequence[3])+String(sequence[4])+String(sequence[5]);
+     finals[2]=String(sequence[6])+String(sequence[7])+String(sequence[8]);
+     finals[3]=String(sequence[9])+String(sequence[10])+String(sequence[11]);
+     finals[4]=String(sequence[12])+String(sequence[13])+String(sequence[14]);
+     
+      Serial.println(finals[0]+" "+finals[1]+" "+finals[2]+" "+finals[3]+" "+finals[4]);
+      SetPosition(servoNumOne,finals[0].toInt());
+      SetPosition(servoNumTwo,finals[1].toInt());
       
-      thrustOne.write(inputs[2]);
-      thrustTwo.write(inputs[3]);
-      thrustThree.write(inputs[4]);
+      thrustOne.write(finals[2].toInt());
+      thrustTwo.write(finals[3].toInt());
+      thrustThree.write(finals[4].toInt());
+      }
+      else{
+       Serial.println(Serial.read()); 
+      }
+
     }
-      
-    
-   int* arrayToInt(int list[]){
-     String int1 = (String)list[0]+(String)list[1]+(String)list[2];
-     int servoOne=int1.toInt();
-     
-     String int2 = (String)list[3]+(String)list[4]+(String)list[5];
-     int servoTwo=int1.toInt();
-     
-     String int3 = (String)list[6]+(String)list[7]+(String)list[8];
-     int thrustOne=int1.toInt();
-     
-     String int4 = (String)list[9]+list[10]+list[11];
-     int thrustTwo=int1.toInt();
-     
-     String int5 = (String)list[12]+list[13]+list[14];
-     int thrustThree=int1.toInt();
-     
-     int returnVal[5] = {servoOne, servoTwo, thrustOne, thrustTwo, thrustThree};
-     return returnVal;
-   }
+
