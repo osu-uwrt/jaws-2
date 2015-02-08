0905eebcf77922086e333d7992ffedd90fabaa8e
tstrick28 <strickland.120@osu.edu>
2015-02-08 15:15:11 -0500

    Used Vector3 to publish the powers.

--- src/controls_node.cpp
+++ src/controls_node.cpp
@@ -1,6 +1,7 @@
 #include "ros/ros.h"
 #include "std_msgs/Float32.h"
 #include "sensor_msgs/Joy.h"
+#include "geometry_msgs/Vector3.h"
 
 class PS3Controller
 {
@@ -8,52 +9,36 @@ private:
   ros::NodeHandle nh;
   ros::Publisher pub_angle_stbd; //publisher for stbd angle
   ros::Publisher pub_angle_port; //publisher for port angle
-  ros::Publisher pub_power_aftt; //publisher for aft power
-  ros::Publisher pub_power_stbd; //publisher for stbd power
-  ros::Publisher pub_power_port; //publisher for port power
+  ros::Publisher pub_power_vect; //publish the power vector
   ros::Subscriber sub;           //subscriber for joy
   std_msgs::Float32 angle_stbd;  //angle of stbd servo
   std_msgs::Float32 angle_port;  //angle of port servo
-  std_msgs::Float32 power_aftt;  //power of aft thruster
-  std_msgs::Float32 power_port;  //power of port thruster
-  std_msgs::Float32 power_stbd;  //Power of stbd thruster
-
+  geometry_msgs::Vector3 power_vect;  //creates vector of powers
 public:
   PS3Controller() : nh()
   {
     pub_angle_stbd = nh.advertise<std_msgs::Float32>("angle_stbd", 1);
     pub_angle_port = nh.advertise<std_msgs::Float32>("angle_port", 1);
-    pub_power_aftt = nh.advertise<std_msgs::Float32>("power_aftt", 1);
-    pub_power_stbd = nh.advertise<std_msgs::Float32>("power_stbd", 1);
-    pub_power_port = nh.advertise<std_msgs::Float32>("power_port", 1);
+    pub_power_vect = nh.advertise<geometry_msgs::Vector3>("power_vect", 1);
     sub = nh.subscribe<sensor_msgs::Joy>("joy", 1, &PS3Controller::callback, this);
   }
   void callback(const sensor_msgs::Joy::ConstPtr& joy)
   {
     float thrust = joy->axes[1] * -127.0; //thruster power
-    power_port.data = thrust; //power of port thruster
-    power_stbd.data = thrust; //power of stbd thruster
-    power_aftt.data = joy->axes[3] * -127.0; //Power for the aft thruster
-    angle_stbd.data = 90 + joy->axes[2] * -90; //Not completely sure if the
-    angle_port.data = 90 + joy->axes[2] * -90;  //angles are correct...
+    float power_port = thrust; //power of port thruster
+    float power_stbd = thrust; //power of stbd thruster
+    float power_port_yaw = joy->axes[8] * -127.0; //"power" of port for yaw
+    float power_stbd_yaw = joy->axes[9] * -127.0; //"power" of stbd for yaw
 
-    float powerPY = joy->axes[8] * -127.0; //"power" of port for yaw
-    float powerSY = joy->axes[9] * -127.0; //"power" of stbd for yaw
-
-    if (powerPY > 0.0)
-    {
-     power_port -= powerPY; //makes port thruster dec as LT inc
-    }
-    if (powerSY > 0.0)
-    {
-     power_aft -= powerSY; //makes stbd thruster dec as RT inc
-    }
+    angle_stbd.data = 90 + joy->axes[2] * 90; //angle of stbd thruster
+    angle_port.data = 90 + joy->axes[2] * 90; //angle of port thruster
+    power_vect.x = joy->axes[3] * -127.0; //power for the aft thruster in x
+    power_vect.y = power_port - power_port_yaw; //power for port thruster y
+    power_vect.z = power_stbd - power_stbd_yaw; //power for stbd thruster z
 
     pub_angle_stbd.publish(angle_stbd);
     pub_angle_port.publish(angle_port);
-    pub_power_aftt.publish(power_aftt);
-    pub_power_stbd.publish(power_stbd);
-    pub_power_port.publish(power_port);
+    pub_power_vect.publish(power_vect);
   }
   void loop()
   {
