fe424058985126a94cbf54c45fef29d5322798fd
Conor Cary <cary.42@osu.edu>
2015-02-25 21:17:33 -0500

    Tested the night of February the 25th in the year of our Robot 2015.

--- src/arbotix_node.cpp
+++ src/arbotix_node.cpp
@@ -10,15 +10,16 @@ class Arbotix
     boost::asio::io_service i_o;
     boost::asio::serial_port s_p;
   public:
-    Arbotix() : nh(), i_o(), s_p(i_o, "/dev/ttyACM0")
+    Arbotix() : nh(), i_o(), s_p(i_o, "/dev/ttyUSB0")
     {
+	s_p.set_option(boost::asio::serial_port_base::baud_rate(38400));
       sub = nh.subscribe<jaws_msgs::Thrusters>("thrusters", 1, &Arbotix::callback, this);
     }
     void callback(const jaws_msgs::Thrusters::ConstPtr& thrusters)
     {
       int size = 11;
       unsigned char d[size];
-
+	char c;
       d[0] = '-';
       d[1] = thrusters->port_angle & 0xFF;
       d[2] = (thrusters->port_angle >> 8) & 0xFF;
@@ -34,9 +35,15 @@ class Arbotix
 
       d[9] = thrusters->aft_power & 0xFF;
       d[10] = (thrusters->aft_power >> 8) & 0xFF;
-
       s_p.write_some(boost::asio::buffer(&d, size));
-    }
+while(1){
+	boost::asio::read(s_p,boost::asio::buffer(&c,1));
+	if(c=='\n'){
+		break;
+	}
+}
+ 
+   }
     void loop()
     {
       ros::spin();
