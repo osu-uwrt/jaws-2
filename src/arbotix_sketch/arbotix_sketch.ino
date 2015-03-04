671d66a9b7db007bd7a13b1872ffc75d5bb2b424
Conor Cary <cary.42@osu.edu>
2015-03-03 21:38:27 -0500

    FML, Sunday.

--- src/arbotix_sketch/arbotix_sketch.ino
+++ src/arbotix_sketch/arbotix_sketch.ino
@@ -17,7 +17,7 @@ const int ANGLE_CONVERSION = 4096/360;
 
 void setup()
 {
-  Serial.begin(38400);
+  Serial.begin(9600);
 
   SetPosition(PORT_SERVO, 90 * ANGLE_CONVERSION);
   SetPosition(STBD_SERVO, 90 * ANGLE_CONVERSION);
@@ -35,18 +35,18 @@ void loop()
     {
       packet[i] = Serial.read();
     }
-    int port_angle = packet[0] * 256 + packet[1];
-    int stbd_angle = packet[2] * 256 + packet[3];
-    int aft_power = packet[4] * 256 + packet[5];
-    int port_power = packet[6] * 256 + packet[7];
-    int stbd_power = packet[8] * 256 + packet[9];
+    int port_angle = (packet[0] << 8) | packet[1];
+    int stbd_angle = (packet[2] << 8) | packet[3];
+    int aft_power = (packet[4] << 8) | packet[5];
+    int port_power = (packet[6] << 8) | packet[7];
+    int stbd_power = (packet[8] << 8) | packet[9];
 
     SetPosition(PORT_SERVO, port_angle * ANGLE_CONVERSION);
     SetPosition(STBD_SERVO, stbd_angle * ANGLE_CONVERSION);
 
-    aft_thruster.write(aft_power * POWER_CONVERSION + 90);
-    port_thruster.write(port_power * POWER_CONVERSION + 90);
-    stbd_thruster.write(stbd_power * POWER_CONVERSION + 90);
+    aft_thruster.write(aft_power);
+    port_thruster.write(port_power);
+    stbd_thruster.write(stbd_power);
   }
 }
 
