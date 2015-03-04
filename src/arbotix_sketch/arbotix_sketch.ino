7bd5956cedf6a803473d3c384274ac72b7b2eafb
Conor Cary <cary.42@osu.edu>
2015-03-04 18:21:10 -0500

    Added a heartbeat.

--- src/arbotix_sketch/arbotix_sketch.ino
+++ src/arbotix_sketch/arbotix_sketch.ino
@@ -15,6 +15,8 @@ Servo stbd_thruster;
 
 const float ANGLE_CONVERSION = 4096.0/360.0;
 
+int heartbeat = 1;
+
 void setup()
 {
   Serial.begin(9600);
@@ -25,12 +27,22 @@ void setup()
   aft_thruster.attach(12);
   port_thruster.attach(14);
   stbd_thruster.attach(13);
+
+  aft_thruster.write(90);
+  port_thruster.write(90);
+  stbd_thruster.write(90);
+  
+  pinMode(0, OUTPUT);
+  digitalWrite(0, heartbeat);
 }
 
 void loop()
 {
   if(Serial.available() > PACKET_SIZE && Serial.read() == '-')
   {
+    heartbeat = !heartbeat;
+    digitalWrite(0, heartbeat);
+
     for(int i = 0; i < PACKET_SIZE; i++)
     {
       packet[i] = Serial.read();
@@ -49,3 +61,4 @@ void loop()
     stbd_thruster.write(stbd_power * POWER_CONVERSION + 90);
   }
 }
+
