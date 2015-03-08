fbe3b5610ad1c5dc11bbf826b7a91ff2ab33b4e5
chilypepper <peter.a.edin@gmail.com>
2015-03-08 11:47:47 -0400

    made some parameters out of glitter

--- src/controls_node.cpp
+++ src/controls_node.cpp
@@ -1,25 +1,34 @@
 #include "ros/ros.h"
 #include "sensor_msgs/Joy.h"
 #include "jaws_msgs/Thrusters.h"
+//#include "string"
 
 class Controls
 {
   private:
     ros::NodeHandle nh;
+    ros::NodeHandle parameters;
     ros::Publisher pub;
     ros::Subscriber sub;
     jaws_msgs::Thrusters thrusters;
+    int stbd_thrust_mult;
+    int port_thrust_mult;
+    int refresh_rate;
   public:
     Controls() : nh()
     {
       sub = nh.subscribe<sensor_msgs::Joy>("joy", 1, &Controls::callback, this);
       pub = nh.advertise<jaws_msgs::Thrusters>("thrusters", 1);
+      nh.param("stbd_thrust_multiplier",stbd_thrust_mult,1);
+      nh.param("port_thrust_multiplier",port_thrust_mult,1);
+      nh.param("controls_refresh_rate",refresh_rate,10);
+
     }
     void callback(const sensor_msgs::Joy::ConstPtr& joy)
     {
       float raw_thrust = joy->axes[1] * 127.0;
-      float stbd_power = raw_thrust;
-      float port_power = raw_thrust;
+      float stbd_power = raw_thrust*nh.getParam("stbd_thrust_multiplier",stbd_thrust_mult);
+      float port_power = raw_thrust*nh.getParam("port_thrust_multiplier",port_thrust_mult);
       float stbd_yaw = joy->axes[13] * -127.0;
       float port_yaw = joy->axes[12] * -127.0;
 
@@ -33,18 +42,22 @@ class Controls
     }
     void loop()
     {
-      ros::Rate rate(10);
+      
+      ros::Rate rate(refresh_rate);
       while(ros::ok())
       {
         ros::spinOnce();
         rate.sleep();
       }
+      nh.getParam("controls_refresh_rate",refresh_rate);
     }
 };
 
 int main(int argc, char **argv)
 {
   ros::init(argc, argv, "controls_node");
+
   Controls controls;
+ 
   controls.loop();
 }
