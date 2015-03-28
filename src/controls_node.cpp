836b6719d7f4392dd1d25c0dbec16c9ef617532c
Conor Cary <cary.42@osu.edu>
2015-03-27 20:41:12 -0400

    Fixed D-Pad, added Aft Thruster, floats became doubles.

--- src/controls_node.cpp
+++ src/controls_node.cpp
@@ -12,24 +12,23 @@ class Controls
     ros::Publisher pub;
     ros::Subscriber sub;
     jaws_msgs::Thrusters thrusters;
-    int stbd_thrust_mult_pull;
-    int port_thrust_mult_pull;
-    float stbd_thrust_mult;
-    float port_thrust_mult;
     int refresh_rate;
+    double aft_thrust_mult;
+    double stbd_thrust_mult;
+    double port_thrust_mult;
 
   public:
     Controls() : nh()
     {
-      nh.param("/controls_node/port_thrust_multiplier", port_thrust_mult_pull, 1);
-      ROS_INFO("Port multiplier: %i", port_thrust_mult_pull);
-      nh.param("/controls_node/stbd_thrust_multiplier", stbd_thrust_mult_pull, 1);
-      ROS_INFO("Starboard multiplier: %i", stbd_thrust_mult_pull);
-      nh.param("/controls_node/controls_refresh_rate", refresh_rate, 10);
+      nh.param<int>("/controls_node/controls_refresh_rate", refresh_rate, 10);
       ROS_INFO("Refresh rate: %i", refresh_rate);
 
-      stbd_thrust_mult=(float) stbd_thrust_mult_pull;
-      port_thrust_mult=(float) port_thrust_mult_pull;
+      nh.param<double>("/controls_node/aft_thrust_multiplier", aft_thrust_mult, 1.0);
+      ROS_INFO("Aft multiplier: %f", aft_thrust_mult);
+      nh.param<double>("/controls_node/port_thrust_multiplier", port_thrust_mult, 1.0);
+      ROS_INFO("Port multiplier: %f", port_thrust_mult);
+      nh.param<double>("/controls_node/stbd_thrust_multiplier", stbd_thrust_mult, 1.0);
+      ROS_INFO("Starboard multiplier: %f", stbd_thrust_mult);
 
       sub = nh.subscribe<sensor_msgs::Joy>("joy", 1, &Controls::callback, this);
       pub = nh.advertise<jaws_msgs::Thrusters>("thrusters", 1);
@@ -44,25 +43,24 @@ class Controls
       float port_yaw = 1 + joy->axes[12];
 
       float aft_thrust = joy->axes[3] * MAX_THRUST;
-      float aft_power = (aft_thrust * aft_thrust * aft_thrust) / (MAX_THRUST * MAX_THRUST); 
+      float aft_power = (aft_thrust * aft_thrust * aft_thrust) / (MAX_THRUST * MAX_THRUST);
 
       thrusters.stbd_angle = (int)(90 + joy->axes[2] * 90);
       thrusters.port_angle = (int)(90 + joy->axes[2] * 90);
       thrusters.aft_power = (int)(1500 + aft_power);
       thrusters.stbd_power = (int)(1500 + stbd_power * stbd_yaw);
       thrusters.port_power = (int)(1500 + port_power * port_yaw);
-      //*** I don't know if the below axes indices are correct. 
-      //*** Supposed to be the D-pad, doesnt matter which.
-      if(joy->axes[5]>0){
+
+      if(joy->buttons[4]){
 	stbd_thrust_mult+=0.001;
       }
-      else if(joy->axes[6]>0){
+      else if(joy->buttons[5]){
  	stbd_thrust_mult-=0.001;
       }
-      if(joy->axes[7]>0){
+      if(joy->buttons[6]){
 	port_thrust_mult+=0.001;
       }
-      else if(joy->axes[8]>0){
+      else if(joy->buttons[7]){
 	port_thrust_mult-=0.001;
       }
 
