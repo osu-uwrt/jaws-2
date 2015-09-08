ae0f05851e3f04e2f6c23eb37e024b951ccc666a
Conor Cary <cary.42@osu.edu>
2015-09-07 20:02:47 -0400

    Serial driver syncs thruster forces and angles.

--- jaws2_hardware/src/serial_port.cpp
+++ jaws2_hardware/src/serial_port.cpp
@@ -1,71 +1,61 @@
 #include "ros/ros.h"
 #include "boost/asio.hpp"
-#include "jaws2_msgs/Thrust.h"
 
+#include "sensor_msgs/JointState.h"
+#include "jaws2_msgs/PwmStamped.h"
 
-class Arbotix
+#include "message_filters/subscriber.h"
+#include "message_filters/time_synchronizer.h"
+
+boost::asio::io_service i_o;
+boost::asio::serial_port s_p(i_o);
+
+void callback(const sensor_msgs::JointState::ConstPtr& angle, const jaws2_msgs::PwmStamped::ConstPtr& force)
 {
-  private:
-    ros::NodeHandle nh;
-    ros::Subscriber sub;
-    ros::Publisher pub;
-    boost::asio::io_service i_o;
-    boost::asio::serial_port s_p;
-    std::string port_name;
-    int baud_rate;
+  const int SIZE = 11;
+  unsigned char packet[SIZE];
 
-  public:
-    Arbotix() : nh(), i_o(), s_p(i_o)
-    {
-      nh.param<std::string>("/arbotix_node/port_name", port_name, "/dev/ttyUSB0");
-      nh.param<int>("/arbotix_node/baud_rate", baud_rate, 9600);
+  packet[0]  = '#';
 
-      s_p.open(port_name);
-      ROS_INFO("Serial port: %s ", port_name.c_str());
-      s_p.set_option(boost::asio::serial_port_base::baud_rate(9600));
-      ROS_INFO("Baud rate: %i", baud_rate);
+  packet[1]  = int(angle->position[0]) >> 8;
+  packet[2]  = int(angle->position[0]);
 
-      sub = nh.subscribe<jaws2_msgs::Thrust>("thrusters", 1, &Arbotix::callback, this);
-      pub = nh.advertise<jaws2_msgs::Thrust>("diagnostic", 1);
-    }
+  packet[3]  = int(angle->position[1]) >> 8;
+  packet[4]  = int(angle->position[1]);
 
-    void callback(const jaws2_msgs::Thrust::ConstPtr& thrust)
-    {
-      const int SIZE = 11;
-      unsigned char packet[SIZE];
+  packet[5]  = force->pwm.aft >> 8;
+  packet[6]  = force->pwm.aft;
 
-      packet[0] = '-';
+  packet[7]  = force->pwm.stbd >> 8;
+  packet[8]  = force->pwm.stbd;
 
-/*
-      packet[1] = thrusters->port_angle >> 8;
-      packet[2] = thrusters->port_angle;
+  packet[9]  = force->pwm.port >> 8;
+  packet[10] = force->pwm.port;
 
-      packet[3] = thrusters->stbd_angle >> 8;
-      packet[4] = thrusters->stbd_angle;
-
-      packet[5] = thrusters->aft_power >> 8;
-      packet[6] = thrusters->aft_power;
-
-      packet[7] = thrusters->port_power >> 8;
-      packet[8] = thrusters->port_power;
-
-      packet[9] = thrusters->stbd_power >> 8;
-      packet[10] = thrusters->stbd_power;
-*/
-
-      s_p.write_some(boost::asio::buffer(&packet, SIZE));
-      pub.publish(thrust);
-    }
-
-    void loop()
-    {
-      ros::spin();
-    }
-};
+  s_p.write_some(boost::asio::buffer(packet, SIZE));
+}
 
 int main(int argc, char **argv)
 {
-  ros::init(argc, argv, "arbotix_node");
-  Arbotix arbotix;
-  arbotix.loop();
+  ros::init(argc, argv, "serial_port");
+
+  std::string port_name;
+  int baud_rate;
+
+  ros::NodeHandle nh;
+
+  nh.param<std::string>("/serial_port/name", port_name, "/dev/ttyUSB0");
+  nh.param<int>("/serial_port/rate", baud_rate, 9600);
+
+  s_p.open(port_name);
+  ROS_INFO("Serial port name: %s", port_name.c_str());
+  s_p.set_option(boost::asio::serial_port_base::baud_rate(baud_rate));
+  ROS_INFO("Serial port rate: %i", baud_rate);
+
+  message_filters::Subscriber<sensor_msgs::JointState> joint_sub(nh, "joint_states", 1);
+  message_filters::Subscriber<jaws2_msgs::PwmStamped> thrust_sub(nh, "thruster_pwm", 1);
+  message_filters::TimeSynchronizer<sensor_msgs::JointState, jaws2_msgs::PwmStamped> sync(joint_sub, thrust_sub, 10);
+  sync.registerCallback(boost::bind(&callback, _1, _2));
+
+  ros::spin();
 }
